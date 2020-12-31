90f1c542cbc80d7394298af454786cf379d1093f
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2020-12-31 18:03:45 +0530

    deploy ssl

--- nirmaan_backend/settings.py
+++ nirmaan_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-#import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -190,7 +190,7 @@ ACCOUNT_FORMS = {
     'disconnect': 'allauth.socialaccount.forms.DisconnectForm',
 }
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '64122815348-4985q5ufld16ljci6pkq1ibl0ebqn4rf.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'MmaZF9jnWMcRrwzoNO6Hhv8E'
